a9439b527b8fbbd3a603f62840e219d3013b0f33
elianmjk <elian.10lyn@gmail.com>
2024-05-20 14:33:16 -0500

    crud api clientes

--- app/Http/Controllers/api/ClienteController.php
+++ app/Http/Controllers/api/ClienteController.php
@@ -3,8 +3,10 @@
 namespace App\Http\Controllers\api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Cliente;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class ClienteController extends Controller
 {
@@ -26,7 +28,30 @@ class ClienteController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $validator = Validator::make($request->all(), [
+            'nombre' => ['required', 'max:255'],
+            'direccion' => ['required', 'max:255'],
+            'categorias_id' => ['required', 'exists:categorias,categorias_id'],
+            'telefono' => ['required', 'max:15'],
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'msg' => 'Se produjo un error en las validaciones de la información',
+                'statuscode' => 422,
+                'errors' => $validator->errors(),
+            ]);
+        }
+
+        $cliente = new Cliente();
+        $cliente->nombre = $request->nombre;
+        $cliente->direccion = $request->direccion;
+        $cliente->categorias_id = $request->categorias_id;
+        $cliente->telefono = $request->telefono;
+        $cliente->save();
+
+        return response()->json(['cliente' => $cliente], 201);
+        
     }
 
     /**
@@ -34,7 +59,16 @@ class ClienteController extends Controller
      */
     public function show(string $id)
     {
-        //
+        $clientes=Cliente::find($id);
+        if(is_null($clientes)){
+            return abort(404);
+        }
+        $categorias=DB::table('categorias')
+        ->orderBy('categorias.nombre')
+        ->get();
+        return response()->json(['clientes'=>$clientes,'categorias'=>$categorias]);
+    
+
     }
 
     /**
@@ -42,7 +76,32 @@ class ClienteController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+        $validator = Validator::make($request->all(), [
+            'nombre' => ['sometimes', 'required', 'max:255'],
+            'direccion' => ['sometimes', 'required', 'max:255'],
+            'categorias_id' => ['sometimes', 'required', 'exists:categorias,categorias_id'],
+            'telefono' => ['sometimes', 'required', 'max:15'],
+        ]);
+        if ($validator->fails()) {
+            return response()->json([
+                'msg' => 'Se produjo un error en las validaciones de la información',
+                'statuscode' => 422,
+                'errors' => $validator->errors(),
+            ]);
+        }
+        $cliente = Cliente::find($id);
+
+        if (!$cliente) {
+            return response()->json(['message' => 'Cliente no encontrado'], 404);
+        }
+
+        $cliente->nombre = $request->nombre ?? $cliente->nombre;
+        $cliente->direccion = $request->direccion ?? $cliente->direccion;
+        $cliente->categorias_id = $request->categorias_id ?? $cliente->categorias_id;
+        $cliente->telefono = $request->telefono ?? $cliente->telefono;
+        $cliente->save();
+
+        return response()->json(['cliente' => $cliente]);
     }
 
     /**
@@ -50,6 +109,19 @@ class ClienteController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        $cliente = Cliente::find($id);
+        
+        if (is_null($cliente)) {
+            return abort(404);
+        }
+
+        $cliente->delete();
+
+        $clientes = DB::table('clientes')
+            ->join('categorias', 'clientes.categorias_id', '=', 'categorias.categorias_id')
+            ->select('clientes.*', 'categorias.nombre as categoria_nombre')
+            ->get();
+
+        return response()->json(['clientes' => $clientes, 'success' => true]);
     }
 }
